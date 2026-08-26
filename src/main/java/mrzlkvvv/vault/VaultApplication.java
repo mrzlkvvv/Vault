7335e090ef32df0ee56b64adbfb00ea979f50158
Kirill Merzlyakov <kiri11.mer31yak0v@gmail.com>
2026-08-26 22:54:36 +0700

    init

---
+++ src/main/java/mrzlkvvv/vault/VaultApplication.java
@@ -0,0 +1,13 @@
+package mrzlkvvv.vault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaultApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(VaultApplication.class, args);
+    }
+
+}
